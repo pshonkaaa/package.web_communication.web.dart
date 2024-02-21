c6299f7b3d6ca4c373c51171d9002745da708248
TrueCollaboration <pshonka.dev@gmail.com>
2024-02-21 18:56:30 +0100

    pkg up

--- lib/src/internal/bases/client.dart
+++ lib/src/internal/bases/client.dart
@@ -1,4 +1,4 @@
-import 'package:true_core/library.dart';
+import 'package:foundation/library.dart';
 import 'package:web_communication/library.dart';
 import 'package:web_communication/src/internal/_internal.dart';
 import 'package:web_communication/src/internal/constants.dart';
